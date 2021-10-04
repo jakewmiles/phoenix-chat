db6a69bcae8262efa2f20cb2c32ac4fdfc722628
jakewmiles <jake.w.miles@gmail.com>
2021-10-04 16:11:56 +0100

    feat: append messages to the dom

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -7,12 +7,20 @@ defmodule ChatWeb.RoomLive do
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
     ChatWeb.Endpoint.subscribe(topic)
-    {:ok, assign(socket, room_id: room_id, topic: topic, messages: ["Hello Twitch!"])}
+    {:ok, assign(socket, room_id: room_id, topic: topic, messages: ["Twitch joined the chat!", "How are things?"])}
   end
 
+  @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
     Logger.info(message: message)
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
     {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    Logger.info(payload: message)
+    {:noreply, assign(socket, messages: socket.assigns.messages ++ [message])}
   end
   
 end
