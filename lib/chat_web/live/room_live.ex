8fc61e1bb3aff55a2cdd2096837626dafccb0941
jakewmiles <jake.w.miles@gmail.com>
2021-10-04 12:18:58 +0100

    feat: display mock message

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -5,7 +5,14 @@ defmodule ChatWeb.RoomLive do
 
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok, assign(socket, room_id: room_id)}
+    topic = "room:" <> room_id
+    ChatWeb.Endpoint.subscribe(topic)
+    {:ok, assign(socket, room_id: room_id, topic: topic, messages: ["Hello Twitch!"])}
+  end
+
+  def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
+    Logger.info(message: message)
+    {:noreply, socket}
   end
   
 end
