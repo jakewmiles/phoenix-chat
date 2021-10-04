81d373c1bb32b7b5f31da21d7b6950aed9753f34
jakewmiles <jake.w.miles@gmail.com>
2021-10-04 18:51:06 +0100

    feat: notify presence (joins and leaves)

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -7,7 +7,10 @@ defmodule ChatWeb.RoomLive do
   def mount(%{"id" => room_id}, _session, socket) do
     topic = "room:" <> room_id
     username = MnemonicSlugs.generate_slug(2)
-     if connected?(socket), do: ChatWeb.Endpoint.subscribe(topic)
+    if connected?(socket) do 
+      ChatWeb.Endpoint.subscribe(topic)
+      ChatWeb.Presence.track(self(), topic, username, %{})
+    end
     {:ok, assign(
       socket, 
       room_id: room_id, 
@@ -34,6 +37,21 @@ defmodule ChatWeb.RoomLive do
   @impl true
   def handle_info(%{event: "new-message", payload: message}, socket) do
     {:noreply, assign(socket, messages: [message])}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff", payload: %{joins: joins, leaves: leaves}}, socket) do
+    Logger.info(joins: joins, leaves: leaves)
+    join_messages =
+      joins
+      |> Map.keys()
+      |> Enum.map(fn username -> %{uuid: UUID.uuid4(), content: "#{username} joined", username: "system"} end)
+
+    leave_messages =
+      leaves
+      |> Map.keys()
+      |> Enum.map(fn username -> %{uuid: UUID.uuid4(), content: "#{username} left", username: "system"} end)
+    {:noreply, assign(socket, messages: join_messages ++ leave_messages)}
   end
   
 end
